aea7342cb420dc36ddaa894f33cc58773515bed3
lili7 <lili7achatova@gmail.com>
2025-03-24 17:11:23 +0300

    PetStoreTests

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print("hello")
+import requests
+
+
+response = requests.get("https://petstore.swagger.io/v2/pet/1")
+
+print(response.status_code)
+print(response.text)
+
+print("1___________")
+
+url = 'https://petstore.swagger.io/v2/pet'
+data =  {
+    "id": 26790,
+    "category": {
+        "id": 1,
+        "name": "string"
+    },
+    "name": "Lilcat",
+    "photoUrls": [
+        "string"
+    ],
+    "tags": [
+        {
+            "id": 1,
+            "name": "string"
+        }
+    ],
+    "status": "available"
+}
+response = requests.post(url, json=data)
+print(response.status_code)
+print(response.text)
+
+print("___________")
+
+response = requests.get('https://petstore.swagger.io/v2/pet/26790')
+
+print(response.status_code)
+print(response.text)
+
+
